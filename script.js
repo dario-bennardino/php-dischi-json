024f03f599da0de79b6800d6d74a7186eb322ffd
dario.bennardino@gmail.com <dario.bennardino@gmail.com>
2024-04-18 13:05:18 +0200

    [add] funzione add new disco

--- script.js
+++ script.js
@@ -23,6 +23,17 @@ createApp({
                     this.list = result.data;
                     console.log(this.list);
                 })
+        },
+
+        addNewDisco() {
+            /*
+            1. strutturo i dati per inviarli in POST al server.php
+            2. invio con axios in post i dati
+            3. con i dati che ricevo aggiorno la lista dei dischi
+            
+            */
+
+            console.log(this.newDisk);
         }
     },
     mounted() {
